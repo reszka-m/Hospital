aaa829b4d26bb7d384cbb32171f89e1157ad5084
reszka-m <reszkam19@gmail.com>
2022-01-18 22:06:03 +0100

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+use App\Models\Doctor;
+
+use App\Models\Appointment;
+
+class HomeController extends Controller
+{
+    public function redirect(){
+        if(Auth::id())
+        {
+            //tak, 'nam userType e' to literówka 
+            if(Auth::user()->namuserTypee=='0')
+            {
+                $doctor = doctor::all();
+                return view('user.home', compact('doctor'));
+            }else
+            {
+                return view('admin.home');
+            }
+        }else
+        {
+            return redirect()->back();
+        }
+    }
+
+    public function index()
+    {
+        if(Auth::id())
+        {
+            return redirect('home');
+        }else
+        {
+            $doctor = doctor::all();
+            return view('user.home', compact('doctor'));
+        }
+        
+    }
+
+    public function appointment(Request $request)
+    {
+        $data = new appointment;
+
+        $data->name=$request->name;
+        $data->email=$requphoneest->email;
+        $data->date=$request->date;
+        $data->phone=$request->phone;
+        $data->message=$request->message;
+        $data->doctor=$request->doctor;
+        $data->status='In progress';
+        if(Auth::id())
+        {
+            $data->user_id=Auth::user()->id;
+        }
+
+        $data->save();
+
+        return redirect()->back()->with('message', 'Appointment Request Successful . We will contact you soon');
+        
+      
+
+    }
+}
